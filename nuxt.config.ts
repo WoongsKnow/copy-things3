fee7b91aa0c39ff7161cdd4c5e002676a56c1a83
woongsknow <woongsknow@gmail.com>
2023-07-26 11:12:22 +0900

    nojekyll 추가

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,13 +2,32 @@
 export default defineNuxtConfig({
   // @ts-ignore
   app: {
-    baseURL: '/copy-things3/'
+    // baseURL: '/copy-things3/',
+    // buildAssetsDir: '_nuxt/',
   },
-  // generate:{
-  //   nojekyll: true, //not working on this version
+  // build: {
+  //   publicPath: '_nuxt/'
   // },
+  router: {
+    base: '/copy-things3/',
+  },
   // ssr: false,
+  // nitro: {
+  //   preset: 'service-worker'
+  // },
   // target: 'static',
+  generate:{
+    nojekyll: true, //not working on this version
+  },
+  // ssr: false,
+  target: 'static',
+  nitro: {
+    output: {
+      dir: '.output',
+      serverDir: '.output/server',
+      publicDir: '.output/public'
+    }
+  },
   css: [
     '@/assets/css/reset.css',
     '@/assets/css/style.css'
